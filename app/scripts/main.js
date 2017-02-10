df18fffe97713f7e3b2b1aef25284d5d86a1da64
Chris Vasquez <chris.vqz@gmail.com>
2017-02-10 17:56:33 -0500

    Updated basic app layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+// When "Day" is clicked
+$('#awf_field-889633520').on('click', function(){
+  $('.hero__bg').removeClass('hero__bg--night');
+  $('.hero__bg').addClass('hero__bg--day');
+});
+
+// When "Night" is clicked
+$('#awf_field-889633521').on('click', function(){
+  $('.hero__bg').removeClass('hero__bg--day');
+  $('.hero__bg').addClass('hero__bg--night');
+});
