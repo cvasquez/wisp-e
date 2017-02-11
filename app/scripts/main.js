a7a8423d2e0852b2f58f697622371efd35ecd0f1
Chris Vasquez <chris.vqz@gmail.com>
2017-02-10 23:26:55 -0500

    Add files via upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,12 @@
 $('#awf_field-889633520').on('click', function(){
   $('.hero__bg').removeClass('hero__bg--night');
   $('.hero__bg').addClass('hero__bg--day');
+  $('#wisp-e-img').attr('src', 'images/wisp-e-day.svg');
 });
 
 // When "Night" is clicked
 $('#awf_field-889633521').on('click', function(){
   $('.hero__bg').removeClass('hero__bg--day');
   $('.hero__bg').addClass('hero__bg--night');
+  $('#wisp-e-img').attr('src', 'images/wisp-e-night.svg');
 });
